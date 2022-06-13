bc4ba3f2bbbb4c86d95e2c0e05ee0d0d9c862ca9
Aleksandr <klim4uk.alex@gmail.com>
2022-06-13 19:11:30 +0300

    Grid

--- src/Game.js
+++ src/Game.js
@@ -4,14 +4,32 @@ class Game extends PIXI.Container {
   constructor() {
     super();
 
-    // GOOGLE: 
     const texture = PIXI.Texture.from('cover.png');
+
     const sprite = new PIXI.Sprite(texture);
-
+    
     this.addChild(sprite);
+    
 
+    const sprite2 = new PIXI.Sprite(texture);
+    sprite2.x = 64;
+    sprite2.y = 64;
+    this.addChild(sprite2);
 
   }
 }
 
 export default Game;
+
+// Сітка 4х4 але як впровадити не знаю
+
+// let result = '';
+// const length = 4; 
+// const weght = 4;
+// for (let i = 1; i <= length; i++){
+//   for (let j = 1; j<= weght; j++){
+//     result += "*";
+//   }
+//   result += "\n";
+// }
+// console.log (result);
